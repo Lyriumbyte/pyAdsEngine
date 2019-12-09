93eff7fd32e216953861248e6a15dd8ea14625c7
Lyrium <carsten.reichhardt@singulus.de>
2019-12-09 17:03:31 +0100

    variable class

---
+++ Variable.py
@@ -0,0 +1,15 @@
+class Variable:
+
+    name = str()
+    indexGroup = int()
+    indexOffset = int()
+    objValue = object()
+    previousValue = object()
+    writeValue = object()
+    isDirty = bool()
+
+    def updateValue (self,newObjValue):
+        if self.objValue != None:
+            self.previousValue = self.objValue
+        self.objValue = newObjValue
+    
