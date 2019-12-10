b5a80c61fe56f68a236b5e56de22e09baa6eb1e8
Reichhardt <Carsten.Reichhardt@singulus.de>
2019-12-10 17:16:48 +0100

    dict in engine

--- src/TcAds/Engine.py
+++ src/TcAds/Engine.py
@@ -1,1 +1,9 @@
-#Contains dict
+class Engine:
+
+    valueDict = {}
+
+    def __init__(self):
+        pass
+    
+    def addValue(self,name,indexGroup,indexOffset,objValue,previousObjectValue,writeValue):
+        self.valueDict [name] = [indexGroup, indexOffset, objValue, previousObjectValue, writeValue]
--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 from TcAds import Variable  as Engine
+from TcAds import Engine  as TcAds
 
-var1 = Engine.Variable("Test")
+TcAds.Variable = Engine.Variable
 
+var1 = TcAds.Variable("Test")
+
+TcAds.Engine.valueDict [var1.name] = [var1.indexOffset, var1.indexGroup]
+
+
+'''
 var1.name = "Hans"
+var1.indexOffset = 121
 print(var1.name)
 var1.writeValue = 5
+'''
 
+print(TcAds.Engine.valueDict)
 
 print(var1)
